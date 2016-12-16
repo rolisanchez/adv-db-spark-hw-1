44e487116de51d4c20ebdb4546a160f28633017b
Victor Rolando Sanchez Jara <vrsj@sd202068.HUNG.ab.nthu.edu.tw>
2016-12-16 18:28:11 +0800

    Part 2 of homework - working with users data

--- python-scripts/adv-db-hw.py
+++ python-scripts/adv-db-hw.py
@@ -78,12 +78,15 @@ def main():
     plt.title('100 stations with the highest average precipitation')
     # plt.xticks(index + bar_width, stationsTop100, rotation=90)
     plt.savefig('/python-scripts/top100precipitations.png')
+    plt.clf()
+
     print "Saved top 100 average precipitations plot to /python-scripts/top100precipitations.png"
 
 
     # Using the Product Rating Dataset provided:
     # What are the records that are rated for item_ID 1-10 from user_ID 1-10,
     # and rating is larger than 3 (not included). Order by rating (descending).
+    # Fields: user_ID,itemr_ID,rating,timestamp
 
     print "Working on user ratings data"
 
@@ -95,18 +98,58 @@ def main():
 
     # Work on user ratings dataset
 
-    # userRatingsFiltered = userParse.filter(lambda line: line!=small_movies_raw_data_header).map(lambda line: line.split(",")).map(lambda tokens: (tokens[0],tokens[1])).cache()
-    # userRatingsFiltered = userParse.filter(lambda x: (x[0],x[1])).cache()
     # userRatingsFiltered = userParse.filter(lambda x: (x[0],x[1],x[2]))
-    # users1to10 = userParse.filter(lambda x: (x[0]>=1 and x[0]<=10))
-    # users1to10 = userParse.filter(lambda x: (int(x[0])<=10))
+    usersItemRatings1to10 = userParse.filter(lambda x: (x[0] != "user_ID" and (int(x[0])<=10) and (int(x[0])>=1) and (int(x[1])<=10) and (int(x[1])>=1) and (int(x[2])>3)))
+
+    usersItemRatingMap = usersItemRatings1to10.map(lambda x : (int(x[0]), (int(x[1]), int(x[2]))))
 
     # users1to10 = userParse.filter(lambda x: (x[0]))
 
-    print "users1to10"
-    print users1to10.take(3)
+    print "usersItemRatings1to10"
+    print usersItemRatings1to10.take(3)
 
-    # print "userRatingsFiltered"
-    # print userRatingsFiltered.take(3)
+    print "usersItemRatingMap"
+    print usersItemRatingMap.take(3)
+
+    print "Will organize users descending from 10 to 1 and then organize their ratings in descending order"
+    print "Notice that some users between 1 and 10 aren't present because they have no items between the 1-10 range provided"
+
+    f2 = open('/python-scripts/usersDecreasingThenRatingsDecreasing.txt', 'w')
+    elementsCount = usersItemRatingMap.count()
+    for element in usersItemRatingMap.map(lambda (user, itemRating): (user, itemRating[1], itemRating[0] )).top(elementsCount):
+        strWrite = "User %d gave a rating of %d to item %d \n" % (element[0], element[1], element[2])
+        f2.write(strWrite)
+
+    f2.close()
+
+    print "Saved users ratings for items to /python-scripts/usersDecreasingThenRatingsDecreasing.txt"
+
+    print "Or we could organize by rating first, then by item, and lastly add the user information:"
+
+    f3 = open('/python-scripts/ratingsDecreasingThenItemsThenUsers.txt', 'w')
+
+    ratingsTop = []
+    itemsTop   = []
+    for element in usersItemRatingMap.map(lambda (user, itemRating): (itemRating[1], itemRating[0], user )).top(elementsCount):
+        strWrite = "Rating of %d for item %d from user %d \n" % (element[0], element[1], element[2])
+        ratingsTop.append(element[0])
+        itemsTop.append(element[1])
+        f3.write(strWrite)
+
+    f3.close()
+    print "Saved descreasing ratings for items to /python-scripts/ratingsDecreasingThenItemsThenUsers.txt"
+
+
+    index = np.arange(elementsCount)
+    plt.bar(index, ratingsTop, bar_width,
+    color='r')
+    plt.xlabel('Items')
+    plt.ylabel('Ratings')
+    plt.title('Items with top ratings')
+    plt.xticks(index + bar_width, itemsTop, rotation=90)
+    plt.savefig('/python-scripts/topRatingsItems.png')
+    print "Saved top ratings for items plot to /python-scripts/topRatingsItems.png"
+
+    print "END"
 if __name__ == "__main__":
         main()
